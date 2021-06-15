b8134d38ce3e4f1932ca09c2fa4cbb2a77c06b6b
ArsenyVekshin <44843792+ArsenyVekshin@users.noreply.github.com>
2021-06-15 09:58:41 +0500

    Update SystemConfig.h

--- board2/SystemConfig.h
+++ board2/SystemConfig.h
@@ -5,8 +5,6 @@
 #define WATER_PIN 24	//пин упраления насосом
 #define COOLER_PIN 23	//пин управления вентилятором
 
-
-
 //ДИСПЛЕЙ
 #define RS_PIN 11	//дисплей пин RS
 #define E_PIN 12	//дисплей пин E
@@ -15,23 +13,23 @@
 #define D6_PIN 3 	//дисплей пин DB6
 #define D7_PIN 2	//дисплей пин DB7
 
-#define DISP_X 16 //количество столбцов дисплея
-#define DISP_Y 2 //количество строк дисплея
+#define DISP_X 16 	//количество столбцов дисплея
+#define DISP_Y 2 	//количество строк дисплея
 
 
 //ДАТЧИК ТЕМПЕРАТУРЫ
 #define TERM_PIN 0	//пин датчика температуры АНАЛОГ!
 
-#define TEMP_CORECT_RES 100000
-#define RESIST_BASE 102000
-#define B_COEF 3950
-#define TEMP_BASE 25
+#define TEMP_CORECT_RES 100000 	//Сопротивление термопары
+#define RESIST_BASE 102000 		//сопротивление резистора делителя
+#define B_COEF 3950 			//коофицент В (даташит)
+#define TEMP_BASE 25 			//температура "обычного" сопротивления (даташит)
 
 //ПРОЧИЕ КОНСТАНТЫ
 #define UP 1
 #define DOWN 0
 
-#define BUTTON 0
-#define TEMPERATURE 1
+#define BUTTON 0 		//тип датчика "кнопка"
+#define TEMPERATURE 1 	//тип датчика "датчик температуры"
 
 #define rattle_k 5 //коофицент для защиты от дребезга кнопок
